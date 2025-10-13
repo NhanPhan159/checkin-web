ae4162e8797dabe493ebde362c83a2127668c0dc
NhanPhan159 <nhandut2019@gmail.com>
2025-10-13 21:57:14 +0700

    fix: change base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"http://localhost:5173",
   define: {
     __APP_VERSION__: JSON.stringify('v1.0.0'),
     __API_URL__: 'window.__backend_api_url',
