6b9c5b275cb848688ed425554e18e301cc1fc6bc
NhanPhan159 <nhandut2019@gmail.com>
2026-03-16 14:07:31 +0700

    change version of tailwind to v3

---
+++ src/modules/QRScan/ScanResult.tsx
@@ -0,0 +1,10 @@
+const ScanResult = () => {
+  return (
+    <>
+      <div className="size-screen m-atuo">
+        <div className="m-1/4 h-1/2 rounded-full shadow-xl bg-white"></div>
+      </div>
+    </>
+  );
+};
+export default ScanResult;
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -9,7 +8,7 @@ export default defineConfig({
     __APP_VERSION__: JSON.stringify("v1.0.0"),
     __API_URL__: "window.__backend_api_url",
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
